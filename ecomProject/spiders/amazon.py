849e60f385b83ee0eafaba13a000442e8638d271
Nirmalkumar Sathiamurthi <arniefan@gmail.com>
2017-07-04 17:46:55 +0530

    added scraper for parsing amazon brands from brand page and also the paginated results

--- ecomProject/spiders/amazon.py
+++ ecomProject/spiders/amazon.py
@@ -6,10 +6,13 @@ class AmazonSpider(scrapy.Spider):
 	name = "amazon"
 	allowed_domains = ["www.amazon.in"]
 	start_urls = [
-			'http://www.amazon.in/gp/site-directory/',
+			# 'http://www.amazon.in/gp/site-directory/',
+			# 'http://www.amazon.in/gp/search/other/ref=lp_1968120031_sa_p_89?rh=n%3A1571271031%2Cn%3A%211571272031%2Cn%3A1968024031%2Cn%3A1968120031&bbn=1968120031&pickerToList=lbr_brands_browse-bin&ie=UTF8&qid=1499164967'
+			'http://www.amazon.in/s/ref=sr_in_-2_p_89_2?fst=as%3Aoff&rh=n%3A976392031%2Cn%3A%21976393031%2Cn%3A1375424031%2Cp_89%3AApple&bbn=1375424031&ie=UTF8&qid=1499165997&rnid=3837712031'			
 			]
 
-	def parse(self, response):
+	# def parse(self,response):
+	def parse1(self, response):
 		q = '//div[contains(@class,"popover-grouping")]//a[contains(@class,"nav_a")]'
 		global site
 
@@ -27,6 +30,7 @@ class AmazonSpider(scrapy.Spider):
 			# break
 
 
+	# def parse(self,response):
 	def parseCategoryPage(self, response):
 		src = response.meta['src']
 		try:
@@ -40,12 +44,33 @@ class AmazonSpider(scrapy.Spider):
 			pass
 		
 
-	def parseProducts(self,response):
+	# def parse(self,response):
+	# # def parseProducts(self,response):
+	# 	q = '//li[contains(@id,"result_")]'
+	# 	for i in response.xpath(q):
+	# 		title = i.xpath('i.//h2').extract()
+			
+	# 	pass
 
-		pass
-
-	def parseBrandsPaginate(self,response):
+	# def parse(self,response):
+	def parseBrandsPages(self,response):
+		global site
 		q = '//span[@class="pagnLink"]/a/@href'
 		brands = response.xpath(q).extract()
-		brands = list(set(brands))
-		yield brands
+		self.logger.debug('parsed the brands pages ')
+		# self.logger.debug(brands)
+		brand_pages = [ site+i for i in list(set(brands))]
+		yield {"brand_pages" : brand_pages}
+
+
+	def parse(self,response):
+	# def parseBrands(self,response):
+		global site
+		q = '//span[@class="refinementLink"]'
+		self.logger.debug('parsing the brands in the page')
+		for i in response.xpath(q):
+			url = site+''.join(i.xpath('../@href').extract())
+			name = ''.join(i.xpath('./text()').extract())
+			cnt = ''.join(i.xpath('./following-sibling::span/text()').extract())[2:-1]
+			# print url,name,cnt
+			yield { "url":url, "brand":name, "count":cnt}
